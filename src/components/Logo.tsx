a73a7a7cb426103b4530d10d7ec627cf83656099
Brinis Abderahmen <brinisabderahmen@gmail.com>
2025-04-18 08:30:03 +0100

    Initial commit

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ function Logo() {
         <div className="flex items-center flex-nowrap flex-shrink-0 flex-grow-0">
             <img src={logo} alt="logo" height="40" width="40" />
             <h1 className="text-xl font-[800] uppercase bg-gradient-to-r from-[#7A8FFA] to-[#E57AEE] bg-clip-text text-transparent">
-                battoutaXbunbun
+                Movix
             </h1>
         </div>
     );
