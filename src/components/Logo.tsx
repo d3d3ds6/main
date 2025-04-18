9288952ecd5d4e621494a59ab18507acd3cd3970
Brinis Abderahmen <brinisabderahmen@gmail.com>
2025-04-18 10:37:10 +0100

    Initial commit

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,14 @@
 import logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
 
 function Logo() {
     return (
-        <div className="flex items-center flex-nowrap flex-shrink-0 flex-grow-0">
+        <Link to="/" className="flex items-center flex-nowrap flex-shrink-0 flex-grow-0 no-underline">
             <img src={logo} alt="logo" height="40" width="40" />
-            <h1 className="text-xl font-[800] uppercase bg-gradient-to-r from-[#7A8FFA] to-[#E57AEE] bg-clip-text text-transparent">
+            <h1 className="text-xl font-extrabold uppercase bg-gradient-to-r from-[#7A8FFA] to-[#E57AEE] bg-clip-text text-transparent">
                 Movix
             </h1>
-        </div>
+        </Link>
     );
 }
 
